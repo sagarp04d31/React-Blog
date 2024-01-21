5a7380a1893bc0f25190ec86b34a70da23cc2df7
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2024-01-21 08:55:06 +0545

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div class="text-center my-10">
+      <h1 class="text-4xl font-serif"> Click to Add </h1>
+      <div>
+        <button 
+          class="px-10 my-20 rounded-md text-2xl font-mono border-2 transition-all 
+          shadow-md hover:shadow-lg duration-300
+          "
+          onClick={() => setCount((count) => count + 1)}
+        >
+          {count}
+        </button>
+        <br />
+      </div>
+    </div>
+  )
+}
+
+export default App
