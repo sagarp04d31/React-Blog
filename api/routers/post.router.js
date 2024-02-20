f6a1305d86e8ca03cc45df07da4ca7aaf0482b90
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2024-02-20 17:37:32 +0545

    get post api

--- api/routers/post.router.js
+++ api/routers/post.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser.js');
-const create = require('../controllers/post.controller.js');
+const { create, getposts } = require('../controllers/post.controller.js');
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/get_posts', getposts)
 
 module.exports = router;
