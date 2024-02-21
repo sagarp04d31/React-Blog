52f64d375c8f1f69be648beee87ccb306d3bfaf3
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2024-02-21 12:52:11 +0545

    delete post functionality

--- api/routers/post.router.js
+++ api/routers/post.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const verifyToken = require('../utils/verifyUser.js');
-const { create, getposts } = require('../controllers/post.controller.js');
+const { create, getposts, deletepost } = require('../controllers/post.controller.js');
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
 router.get('/get_posts', getposts)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 module.exports = router;
