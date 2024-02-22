26a475c14d4ae163f9f9c769755efa632a583f8a
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2024-02-22 23:07:18 +0545

    delete comment functionality

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -78,4 +78,20 @@ const editComment = async (req, res, next) => {
   }
 }
 
-module.exports = { createComment, getPostComments, likeComment, editComment }
+const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+    if (!comment) {
+      return next(errorHandler(404, 'Comment not found'));
+    }
+    if (comment.userId !== req.user.id && !req.user.isAdmin) {
+      return next(errorHandler(403, 'You are not allowed to delete this comment'));
+    }
+    await Comment.findByIdAndDelete(req.params.commentId);
+    res.status(200).json('Comment has been deleted');
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { createComment, getPostComments, likeComment, editComment, deleteComment }
--- api/routers/comment.route.js
+++ api/routers/comment.route.js
@@ -4,7 +4,8 @@ const {
   createComment, 
   getPostComments, 
   editComment,
-  likeComment 
+  likeComment,
+  deleteComment
 } = require('../controllers/comment.controller.js');
 
 const router = express.Router();
@@ -13,5 +14,6 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 module.exports = router;
