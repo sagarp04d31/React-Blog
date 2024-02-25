b71f4be1838d4abe23bae055819f53ae67ff35eb
DESKTOP-K3J2FSU\sagar <programmingforsagar001@gmail.com>
2024-02-25 16:45:37 +0545

    +

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  const port = process.env.PORT || 3000;
-
   server: {
     proxy: {
       "/api": {
-        target: `0.0.0.0:${port}`,
+        target: "0.0.0.0:3000",
         changeOrigin: true,
         secure: false,
         ws: true,
